4df87338ba7af0b538c329f5f04e138d0cef01f7
Asghar Ale <asgharale2021@gmail.com>
2021-03-18 07:50:34 +0330

    comments pagination ans show comments on the frontend

--- light-mag/src/components/comments.vue
+++ light-mag/src/components/comments.vue
@@ -1,6 +1,6 @@
 <template>
-	<p v-if="!comments" class="cen link-like" @click.prevent="get_coms()">نمایش کامنت ها</p>
-	<div v-else class="com-box">
+	<p v-if="comments==false" class="cen link-like" @click.prevent="get_coms(endpoint)">نمایش کامنت ها</p>
+	<div v-else class="com-box" ref="paginte">
 		<div v-for="(com,i) in comments" :key="i" class="com">
 			<h4>{{com.name}}</h4>
 			<time class="s-size">{{com.jdate}}</time>
@@ -18,23 +18,46 @@
 <script>
 	import {ref} from 'vue';
 	import {getAPI} from'@/axios.js';
+	import {onBeforeUnmount} from 'vue';
+	import {useRoute} from 'vue-router';
 	export default{
 		name: "Comments",
 		props:['id'],
 		setup(props){
-			const comments = ref(null);
-			async function get_coms(){
-				try{
-					const res = await getAPI.get("comments/api/v1/"+props.id);
-					comments.value = res.data.results;
-					// for(var i=0;i<comments.value.length;i++){
-					// 	comments.value[i]['replies'] = {
-					// 		next: null,
-					// 		reps: []
-					// 	};
-					// }
-				}catch(err){
-					console.log(err)
+			const comments = ref([]);
+			const endpoint = ref("comments/api/v1/"+props.id);
+			let more = false;
+			let bottom = false;
+			const paginte = ref(null);
+			async function get_coms(end){
+				if(endpoint.value&&end){
+					try{
+						const res = await getAPI.get(end);
+						for(var i=0;i<res.data.results.length;i++){
+							res.data.results[i]['replies'] = {next:null,reps:[]};
+							comments.value.push(res.data.results[i]);
+						}
+						endpoint.value = res.data.next;
+						console.log(endpoint.value);
+						more = true;
+						window.addEventListener('scroll',CaP);
+					}catch(err){
+						console.log(err)
+					}
+				}
+			}
+			function CaP(){
+				return pagination(endpoint.value)
+			}
+			const route = useRoute();
+			async function pagination(end){
+				if(paginte.value){
+					bottom = (window.scrollY+window.innerHeight)>=(paginte.value.offsetTop+paginte.value.offsetHeight-20)&&(window.scrollY+window.innerHeight)<(paginte.value.offsetTop+paginte.value.offsetHeight);
+					console.log(bottom)
+					if(bottom&&more&&end&&route.name==="article"){
+						more = false;
+						get_coms(end);
+					}
 				}
 			}
 			// async function get_reps(pk,i){
@@ -45,7 +68,8 @@
 			// 		console.log(err)
 			// 	}
 			// }
-			return{get_coms,comments}
+			onBeforeUnmount(()=>window.removeEventListener('scroll',CaP))
+			return{get_coms,comments,endpoint,paginte}
 		}
 	};
 </script>
